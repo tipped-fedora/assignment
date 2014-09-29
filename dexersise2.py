a488ab4b5ce9e274040e714627a7ea94855d068f
robert <robert@Robertslaptop>
2014-09-29 08:38:19 +0100

    added 2 assignment exersises

---
+++ dexersise2.py
@@ -0,0 +1,11 @@
+#devolopment exersise2
+#robert Fox
+def main():
+    temprature = int(input("enter the temprature of the liquid: ")
+    if temprature>=100:
+                     print("The liquid is boiling")
+    elif 0<temprature<100:
+        print("The liquid is not boiling or frozen")
+    else:
+        print("The liquid is frozen") 
+
---
+++ exersise1.py
@@ -0,0 +1,10 @@
+#revision exersise1
+#robert fox
+def main():
+    
+    number1 = float(input("please enter your first number: "))
+    number2 = float(input("please enter your second number: "))
+    if number1==number2:
+        print("These numbers are the same")
+    else:
+        print("these numbers are not the same")
